6af001427543af1175783006eecd00bd36b11394
Antoine Buard <a.buard@laposte.net>
2023-03-10 16:07:37 +0100

    Ajout de la page error

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import img404 from "../assets/404.png";
 
 export const Error = () => {
-  return <div>Error</div>;
+  return (
+    <main className="main">
+      <div className="error">
+        <div className="img-container">
+          <img src={img404} alt="a person parachuting" />
+        </div>
+
+        <h1 className="title-error">Error 404</h1>
+        <p className="error-text">La page que vous demandez n'existe pas.</p>
+        <button className="btn-primary">
+          <Link to="/">Retournez à la page d'accueil</Link>
+        </button>
+      </div>
+    </main>
+  );
 };
