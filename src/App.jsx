984a42a8992ce97bd12a2b1f5de46eb0c14a7dd4
Antoine Buard <a.buard@laposte.net>
2023-03-10 15:54:59 +0100

    Ajout de la page Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,23 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { List } from "./pages/List";
+import { Error } from "./pages/Error";
+import { CreateEmployee } from "./pages/CreateEmployee";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export const App = () => {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create-employee" element={<CreateEmployee />} />
+        <Route path="/list-employee" element={<List />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
-}
-
-export default App;
+};
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,1 +1,11 @@
-RTCSctpTransport;
+import React from "react";
+import logo from "../assets/logo.jpg";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="line"></div>
+      <img className="logo-footer" src={logo} alt="Logo WealthHealth" />
+    </footer>
+  );
+};
